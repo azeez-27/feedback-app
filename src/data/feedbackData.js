585142b25ef5e6af1ff822e8cf9fd333962ba292
azeez-27 <abdul.azeez2720@gmail.com>
2023-05-23 13:33:08 +0500

    Adding the final project

---
+++ src/data/feedbackData.js
@@ -0,0 +1,19 @@
+const feedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: 'Good app',
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: 'Better than others',
+  },
+  {
+    id: 3,
+    rating: 6,
+    text: 'Average',
+  },
+]
+
+export default feedbackData
